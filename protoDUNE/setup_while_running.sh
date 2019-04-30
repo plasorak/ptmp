62fdb87a7210861829a13f4a3ec7d7d328e2a344
Pierre Lasorak <plasorak@fnal.gov>
2019-04-30 20:54:15 +0200

    minor

--- protoDUNE/setup_while_running.sh
+++ protoDUNE/setup_while_running.sh
@@ -6,7 +6,7 @@ setup protobuf v3_3_1a -q e15
 
 
 function connect {
-    ./build/test/check_recv --ntpsets 100 -a connect --socket-endpoints $2 --socket-pattern SUB --timeout-ms 1000 &>/dev/null
+    ../build/test/check_recv --ntpsets 100 -a connect --socket-endpoints $2 --socket-pattern SUB --timeout-ms 1000 &>/dev/null
     if [ $? -eq 0 ]
     then
         echo "$1 is sending hits!"
